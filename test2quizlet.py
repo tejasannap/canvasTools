4ea007ce1c3280ceeec52d68bcd503698082f08d
tekkerzzrubot <annapat.reddy@gmail.com>
2023-03-13 09:07:07 -0400

    rework, mostly working

--- test2quizlet.py
+++ test2quizlet.py
@@ -1,133 +1,92 @@
 import re
-import json
 
-file = open("quiz.html", "r", encoding="utf8").read().strip()
+with open("quiz.html", "r", encoding="utf8") as file:
+    file_content = file.read()
 
-questionRegex = r"\"textarea_question_text\">&lt;span style=&quot;font-family: times new roman; font-size: 12pt; color: #000000;  &quot;&gt;([\s\S]*?)&lt;[^\"]span&gt;<"
+# Extract questions
+question_regex = r"\"textarea_question_text\">&lt;span style=&quot;font-family: times new roman; font-size: 12pt; color: #000000;  &quot;&gt;([\s\S]*?)&lt;[^\"]span&gt;<"
+questions = re.findall(question_regex, file_content)
 
-questionMatch = re.finditer(questionRegex, file)
+# Extract answers
+answer_regex = r"id=\"answer_[^\"]*\"\n       style=\"\"\n      title=\"([^\"]*).>"
+answers = re.findall(answer_regex, file_content)
+# print(answers)
 
-answerRegex = (r"id=\"answer_[^\"]*\"\n"
-	r"       style=\"\"\n"
-	r"      title=\"([^\"]*).>")
+# Extract correct answers
+correct_regex = r"title=\"(.*?)(?:\.*?)(?:. This was the correct answer|. You selected this answer. This was the correct answer.)"
+correct_answers = re.findall(correct_regex, file_content)
 
-answerMatch = re.finditer(answerRegex, file)
-#print(list(answerMatch))
+# Create a list of dictionaries, with each dictionary containing a question and its answers
+quiz = []
+tf = False
+minus2 = False 
+x = 0
 
-correctRegex = r"title=\"(.*?)(?:\.*?)(?:. This was the correct answer|. You selected this answer. This was the correct answer.)"
+for question_num, question in enumerate(questions):
 
-correctMatch = re.finditer(correctRegex, file)
+    q = question_num
+    tempans = "{temp}".format(temp = answers[x]).lower()
+
+    if minus2 and not "true" in tempans or not "false" in tempans: 
+        q = q - 2
+        minus2 = False
+
+    # nextans = "{temp}".format(temp = answers[x+4]).lower()
+
+    if "true" in tempans or "false" in tempans:
+        x = x + 2
+        ans = ["True", "False"]
+        print("True/False")
+        minus2 = True
 
 
-questions = {}
+    else: 
+        x = x + 4
+        print("Normal Question")
+        ans = answers[q * 4:(q + 1) * 4]
 
 
-# Add values to the nested dictionary
-for q,  (cMatch, qMatch) in enumerate(zip(correctMatch, questionMatch), start=0):
 
+    # print(x)
+    # print(answers[question_num])
+    # if question_num == 1:
+    #     x = question_num + 1
+    # else:
+    # if tf: 
+    #     x = question_num - 2
+    #     answers_for_question = answers[x * 4:(x + 1) * 4]
+    #     tf = False
+    # else:
+    #     answers_for_question = answers[question_num * 4:(question_num + 1) * 4]
     
-    first = {}
-    
-    # ques = "WHat{b}".format(b = i)
-    ques = qMatch.groups()
-    correct = cMatch.groups()
+    # # Check if any of the answers contain the strings "true" or "false" (case-insensitive)
+    # if any("true" in answer.lower() for answer in answers_for_question) or any("false" in answer.lower() for answer in answers_for_question):
+    #     answers_for_question = ["True", "False"]
+    #     tf = True
+    #     minus2 = True
+    print(question)
+    print(ans)
+    print()
 
-    # for matchNum, match in enumerate(answerMatch, start=1):
-
-    #   print(match.groups())
-  
-    
-    # for x in range(3): 
-    #     answer[x] = "answeris{x}".format(x = x)
-
-    # for x, answerText in enumerate(answerMatch, start=1):
-    #   print(x)
-    #   answer[x] = answerText.groups()
-
-    # group_list = []
-    # aanswer = []
-  
-    # for matchNum, match in enumerate(answerMatch, start=1):
-    #       group = match.groups()
-    #       group_list.extend(group)
-          
-    #       if len(group_list) == 4:
-    #           dictionary = {f"answer_{i+1}": group_list[i] for i in range(4)}
-    #           aanswer.append(dictionary)
-    #           group_list.clear()
-
-    tempanswer = {}
-    answer = {}
-
-    a = 0
-    b = 0
-    tf = False
-
-    for matchNum, match in enumerate(answerMatch, start=0):
-
-        ans = match.groups()
-        ans_str = "{ans}".format(ans = ans)
-        ans_str = ans_str.lower()
-        a = a + 1
-
-
-        if "true" in ans_str or "false" in ans_str:
-            if a == 3:
-              b = b + 1
-              a = 0
-
-              answer[b] = tempanswer
-              tempanswer = {}
-            else:
-                print(match.groups())
-                tempanswer[a] = ans
-
-        elif a == 5:
-            b = b + 1
-            a = 0
-
-            # print(tempanswer)
-            # print()
-            answer[b] = tempanswer
-            tempanswer = {}
-        else:
-            print(match.groups())
-            tempanswer[a] = ans
-      
-    
-        
-    first = { 
-    "question": ques,
-    "correct": correct,
-    "ans": answer
+    answers_for_question = ans
+    correct_answer = correct_answers[question_num]
+    question_dict = {
+        "question": question,
+        "answers": answers_for_question,
+        "correct_answer": correct_answer
     }
-        
-    
-    questions[q] = first
+    quiz.append(question_dict)
 
 
+# Print the quiz
 
-  
-
-# print(questions)
-
-
-# for q in range(len(questions)):
-
-#   print(questions[q])
+def printQuiz(): 
+    for question_num, question_dict in enumerate(quiz, start=1):
+        print(f"Question {question_num}: {question_dict['question']}")
+        for answer_num, answer in enumerate(question_dict['answers'], start=1):
+            print(f"{answer_num}. {answer}")
+        print(f"Correct answer: {question_dict['correct_answer']}\n")
 
 
 
 
-# def write_pairs(pairs: str, location: str):
-#     '''Writes question-and-answer pairs to a text file in the Quizlet tab-separated format'''
-#     with open(location, 'w', encoding="utf8") as f:
-#         f.write(pairs)
-
-
-# write_pairs(questions, "output.txt")
-
-
-
-with open("output.txt", "w") as f:
-    json.dump(questions, f, indent = 4)
