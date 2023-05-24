9e47d34aceb467d24d5927b2abf7977a62d19a7c
Tejas <annapat.reddy@gmail.com>
2023-05-23 22:31:18 -0400

    works!

--- qz.py
+++ qz.py
@@ -1,37 +1,44 @@
 from bs4 import BeautifulSoup
 import re
+import subprocess
+import os
 
-with open("quiz2.html", 'r') as fp:
-    soup = BeautifulSoup(fp, "html.parser")
+def quizlet(file: str):
+    with open(file, 'r') as fp:
+        soup = BeautifulSoup(fp, "html.parser")
 
-    questions = soup.find_all("div", class_="question")
+        questions = soup.find_all("div", class_="question")
 
-    for q in range(len(questions)):
+        for q in range(len(questions)):
 
-        print("------------------------------------------------------------")
-        question = questions[q]
+            question = questions[q]
 
-        question_text = question.find("div", class_="question_text user_content")
-        question_text = question_text.find("span", style="font-family: times new roman; font-size: 12pt; color: #000000; ").text
-        print(question_text)
+            question_text = question.find("div", class_="question_text user_content")
+            question_text = question_text.find("span", style="font-family: times new roman; font-size: 12pt; color: #000000; ").text
+            print("//" + question_text)
 
-        answers = question.find_all("div", class_="answer")
+            answers = question.find_all("div", class_="answer")
 
-        for a in range(len(answers)):
-            answer = answers[a]
+            for a in range(len(answers)):
+                answer = answers[a]
 
-            answer_text = answer.find("span", style="font-family: times new roman; font-size: 12pt; color: #000000; ").text
-            print(answer_text)
+                answer_text = answer.find("span", style="font-family: times new roman; font-size: 12pt; color: #000000; ").text
+                print(answer_text)
 
-        correct = question.find("div", class_="correct_answer")
-        correct_text = correct.find("span", style="font-family: times new roman; font-size: 12pt; color: #000000; ").text
-        print(" ")
-        print(correct_text)
+            correct = question.find("div", class_="correct_answer")
+            correct_text = correct.find("span", style="font-family: times new roman; font-size: 12pt; color: #000000; ").text
+            print("/" + correct_text)
+
+        with open("q.txt", "w", encoding="utf8") as file:
+            file.write(str(questions[19]))
+            # print("File written successfully")
 
 
-        print(" ")
+# quizlet()
 
+# List all files in the folder
+files = os.listdir('quizzes')
 
-    with open("q.txt", "w", encoding="utf8") as file:
-        file.write(str(questions[19]))
-        # print("File written successfully")
+# Print the file names
+for file in files:
+    quizlet("quizzes/" + file)
